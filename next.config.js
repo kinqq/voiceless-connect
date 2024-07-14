ff0591575977f6aa0f0754f75d9f7ad05035aa15
Kinkyu <minkyu070602@gmail.com>
2024-07-15 02:39:01 +0900

    deploy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-// /**
-//  * @type {import('next').NextConfig}
-//  */
-// const nextConfig = {
-//   output: "export",
-//   distDir: "dist",
-//   images: {
-//     unoptimized: true,
-//   },
-// };
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
+};
 
-// module.exports = nextConfig;
+module.exports = nextConfig;
